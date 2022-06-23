49bcdc4b3814f04ea8a48326684048a53af361bf
Sidhe_is_me <yvonnedreptate@gmail.com>
2022-06-23 16:30:02 -0400

    Angular Tutorial
    Section 1 and Section 2 to lesson 24

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,16 @@
+
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+ // styleUrls: ['./app.component.css']
+styles:[`
+h1{
+  color: red;
+/* styles can be definded here in the app.component.ts file inline but only if you do not need that many. If more are needed define them in theapp.component .css file. */
+}
+`]
 })
 export class AppComponent {
 
